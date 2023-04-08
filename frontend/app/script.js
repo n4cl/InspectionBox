079b7b0bf26d1845486854370b1fdbbf56e7a276
n4cl <devn4cl@gmail.com>
2023-04-09 00:28:55 +0900

    Add: テストリクエスト処理を追加

--- frontend/app/script.js
+++ frontend/app/script.js
@@ -1,23 +1,54 @@
-function requestAPI(){
+document.getElementById("btn_request").addEventListener("click", function() {
   const uri = new URL(window.location.href);
-  const request = new XMLHttpRequest();
+  const xhr = new XMLHttpRequest();
   let original_url = uri.origin
   let url = original_url + "/backend"
 
-  request.onreadystatechange = function(){
-    if (request.readyState == 4){
-      if (request.status == 200){
-        let data = request.responseText;
-        console.log(data);
-      }
+  var params = "message=" + encodeURIComponent(document.getElementById("g_p_mtext").value);
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      document.getElementById("g_r_mtext").value = xhr.responseText;
     }
   }
-
-  request.open("GET", url, true);
-  request.send()
-
-}
+  xhr.send(params);
+});
 
 
-let btnRequest = document.getElementById('btn_request');
-btnRequest.addEventListener('click', requestAPI);
+document.getElementById("btn_test_get").addEventListener("click", function() {
+  const uri = new URL(window.location.href);
+  const xhr = new XMLHttpRequest();
+  let original_url = uri.origin
+  let url = original_url + "/backend"
+
+  xhr.open("GET", url, true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      console.log("Get test OK");
+    } else {
+      console.log("Request failed. Returned status: " + xhr.status);
+    }
+  }
+  xhr.send();
+});
+
+document.getElementById("btn_test_post").addEventListener("click", function() {
+  const uri = new URL(window.location.href);
+  const xhr = new XMLHttpRequest();
+  let original_url = uri.origin
+  let url = original_url + "/backend"
+
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      console.log(xhr.responseText);
+    } else {
+      console.log("Request failed. Returned status: " + xhr.status);
+    }
+  }
+  const data = { "message": "Hello" };
+  xhr.send(JSON.stringify(data));
+});
